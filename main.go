0928994977c0dfe8703f3cb926447dee70d6ceda
Tesshu Flower <tflower@redhat.com>
2023-04-12 15:01:01 -0400

    Allow flags to configure leader election settings (#115)
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	goflag "flag"
 
@@ -61,11 +62,27 @@ func newCommand() *cobra.Command {
 }
 
 func newControllerCommand() *cobra.Command {
-	cmd := controllercmd.
-		NewControllerCommandConfig("volsync-addon-controller", getVersion(), runControllers).
-		NewCommand()
+	cmdConfig := controllercmd.
+		NewControllerCommandConfig("volsync-addon-controller", getVersion(), runControllers)
+	cmd := cmdConfig.NewCommand()
 	cmd.Use = "controller"
 	cmd.Short = "Start the volsync addon controller"
+
+	flags := cmd.Flags()
+
+	flags.DurationVar(&cmdConfig.LeaseDuration.Duration, "leader-election-lease-duration", 137*time.Second, ""+
+		"The duration that non-leader candidates will wait after observing a leadership "+
+		"renewal until attempting to acquire leadership of a led but unrenewed leader "+
+		"slot. This is effectively the maximum duration that a leader can be stopped "+
+		"before it is replaced by another candidate. This is only applicable if leader "+
+		"election is enabled.")
+	flags.DurationVar(&cmdConfig.RenewDeadline.Duration, "leader-election-renew-deadline", 107*time.Second, ""+
+		"The interval between attempts by the acting master to renew a leadership slot "+
+		"before it stops leading. This must be less than or equal to the lease duration. "+
+		"This is only applicable if leader election is enabled.")
+	flags.DurationVar(&cmdConfig.RetryPeriod.Duration, "leader-election-retry-period", 26*time.Second, ""+
+		"The duration the clients should wait between attempting acquisition and renewal "+
+		"of a leadership. This is only applicable if leader election is enabled.")
 
 	return cmd
 }

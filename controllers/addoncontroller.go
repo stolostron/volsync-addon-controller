e46260e73a8c40f485be091b0ac79e4104c88cfb
Tesshu Flower <tflower@redhat.com>
2024-09-09 12:39:01 -0400

    update acm-2.12 default to deploy volsync from stable-0.11 channel
    Signed-off-by: Tesshu Flower <tflower@redhat.com>

--- controllers/addoncontroller.go
+++ controllers/addoncontroller.go
@@ -46,10 +46,10 @@ const (
 	operatorName                   = "volsync-product"
 	globalOperatorInstallNamespace = "openshift-operators"
 
-	// Defaults for ACM-2.11
+	// Defaults for ACM-2.12
 	DefaultCatalogSource          = "redhat-operators"
 	DefaultCatalogSourceNamespace = "openshift-marketplace"
-	DefaultChannel                = "stable-0.10" // No "acm-x.y" channel anymore - aligning ACM-2.11 with stable-0.10
+	DefaultChannel                = "stable-0.11" // No "acm-x.y" channel anymore - aligning ACM-2.12 with stable-0.11
 	DefaultStartingCSV            = ""            // By default no starting CSV - will use the latest in the channel
 	DefaultInstallPlanApproval    = "Automatic"
 )
